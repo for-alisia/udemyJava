d9a66e61cf990fcb6c73c50cae8d38297e7a1f3d
Elena <for-alisia@yandex.ru>
2022-12-28 21:45:32 +0100

    Continue with strings

--- src/StringExamples.java
+++ src/StringExamples.java
@@ -12,14 +12,44 @@ public class StringExamples {
         // Java will not check for existence
         String secondString = new String("My second string");
         String anotherSecondString = new String("My second string");
-        System.out.println(secondString == anotherFirstString); // false
+        System.out.println(secondString == anotherSecondString); // false
 
+        StringExamples myEx = new StringExamples();
+
+        // To check info about string methods
+        // go to specific method of this class
+        myEx.workingWithChars();
+        myEx.blankOrEmpty();
+        myEx.replaceText();
+        myEx.comparingStrings();
+        myEx.concatStrings();
+        myEx.containsSubstring();
+        myEx.lengthOfTheString();
+        myEx.removeSpaces();
+        myEx.lowerCase();
+        myEx.upperCase();
+        myEx.substring();
+    }
+
+    private void lengthOfTheString() {
+        String firstString = "My first string";
+        // Length of the string
+        System.out.println(firstString.length());
+    }
+
+    private void upperCase() {
+        String firstString = "My first string";
         // Upper case
-        String myText = "some text";
-        System.out.println(myText.toUpperCase());
+        System.out.println(firstString.toUpperCase());
+    }
+
+    private void lowerCase() {
+        String firstString = "MY STRING";
         // Lower case
-        String myBigText = "TEXT";
-        System.out.println(myBigText.toLowerCase());
+        System.out.println(firstString.toLowerCase());
+    }
+
+    private void blankOrEmpty() {
         // Difference between isEmpty and isBlank
         String empty = "";
         String blank = " ";
@@ -29,12 +59,16 @@ public class StringExamples {
         // Blank - counts meaningful chars, whitespace is not meaningful char
         System.out.println(blank.isBlank()); // true
         System.out.println(empty.isBlank()); // true
+    }
 
+    private void replaceText() {
         // Replacing - case-sensitive
         String replaceText = "My bad idea is bad?";
         System.out.println(replaceText.replace("bad", "good"));
         System.out.println(replaceText.replace('?', '!'));
+    }
 
+    private void removeSpaces() {
         // Remove spaces
         // trim is older and does not support difference between languages
         String withSpace = "  my text  ";
@@ -48,7 +82,9 @@ public class StringExamples {
                 third line
                 """;
         System.out.format("'%s'", multiline.stripIndent());
+    }
 
+    private void workingWithChars() {
         // charAt
         String apple = "apple";
         int length = apple.length();
@@ -57,6 +93,11 @@ public class StringExamples {
         }
         System.out.println(apple.charAt(3));
 
+        // toCharArray()
+        char[] myChars = apple.toCharArray(); // ['a', 'p', 'p', 'l', 'e']
+    }
+
+    private void comparingStrings() {
         // compareTo - returns negative int if first word before in alphabet
         // 0 - the same words
         // positive - if second word earlier alphabetically
@@ -65,9 +106,57 @@ public class StringExamples {
 
         System.out.println(fruit.compareTo(vegetable)); // -1
         System.out.println(fruit.compareToIgnoreCase(vegetable)); // non-case-sensitive
+    }
 
+    private void containsSubstring() {
         // Contains
         String containsMe = "Can anyone hear me?";
         System.out.println(containsMe.contains("me")); // true
+    }
+
+    private void concatStrings() {
+        // String concatenation
+        String concatFirst = "Concat";
+        String concatSecond = "strings";
+
+        // Not efficient - keeps resulting string in memory
+        System.out.println(concatFirst + " " + concatSecond);
+
+        // Will not have a space between
+        System.out.println(concatFirst.concat(concatSecond));
+
+        // With builder - more efficient (in loops for example)
+        // StringBuilder can get a parameter of a length of final string -
+        // StringBuilder(concatFirst.length() + concatSecond.length() + 1)
+        String finalString = new StringBuilder()
+                .append(concatFirst)
+                .append(" ")
+                .append(concatSecond)
+                .toString();
+        System.out.println(finalString);
+
+        // Old way with StringBuffer - less efficient as it is trade safe
+        String finalBuffer = new StringBuffer()
+                .append(concatFirst)
+                .append(" ")
+                .append(concatSecond)
+                .toString();
+        System.out.println(finalBuffer);
+
+        // Format in output
+        System.out.format("%s %s\n", concatFirst, concatSecond);
+
+        // String literals
+        String finalLiteral = String.format("%s %s", concatFirst, concatSecond);
+        System.out.println(finalLiteral);
+    }
+
+    private void substring() {
+        String str = "apple";
+
+        String firstLetter = str.substring(0, 1); // "a"
+        String restPart = str.substring(1); // "pple"
+
+        System.out.println(firstLetter.toUpperCase().concat(restPart));
     }
 }
