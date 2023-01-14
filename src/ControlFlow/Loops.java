16ad95623cb5c7ee2bbede1f99665946623a94d9
Elena <for-alisia@yandex.ru>
2023-01-14 13:15:22 +0100

    Started to add do/while

--- src/ControlFlow/Loops.java
+++ src/ControlFlow/Loops.java
@@ -8,36 +8,56 @@ public class Loops {
         boolean isGuessed = false;
         String guessedNumberStr = null;
 
-        while (!isGuessed && !"q".equals(guessedNumberStr)) {
-           guessedNumberStr = System.console().readLine("Please put a number between 1 and 10 inclusively %n");
+//        while (!isGuessed && !"q".equals(guessedNumberStr)) {
+//           guessedNumberStr = System.console().readLine("Please put a number between 1 and 10 inclusively %n");
+//            if (guessedNumberStr.matches("-?\\d{1,2}")) {
+//                int guessedNumber = Integer.parseInt(guessedNumberStr);
+//
+//                if (guessedNumber == rndNum) {
+//                    System.out.println("You've got it!");
+//                    // Instead of boolean we can just return here (without value) and it breaks the loop
+//                    isGuessed = true;
+//                } else {
+//                    System.out.println("Sorry, try again!");
+//                }
+//            }
+//        }
+
+        // Second option (easier to read) - but app can blow up if user will provide not q or integer
+//        while(true) {
+//            guessedNumberStr = System.console().readLine("Please put a number between 1 and 10 inclusively %n");
+//            if (guessedNumberStr.equals("q")) {
+//                return;
+//            }
+//
+//            int guessedNumber = Integer.parseInt(guessedNumberStr);
+//
+//            if (guessedNumber == rndNum) {
+//                System.out.println("You've got it!");
+//                return;
+//            } else {
+//                System.out.println("Sorry, try again!");
+//            }
+//        }
+
+        int attempts = 0;
+        // With do-while
+        do {
+            guessedNumberStr = System.console().readLine("Please put a number between 1 and 10 inclusively %n");
+
             if (guessedNumberStr.matches("-?\\d{1,2}")) {
                 int guessedNumber = Integer.parseInt(guessedNumberStr);
 
                 if (guessedNumber == rndNum) {
                     System.out.println("You've got it!");
-                    // Instead of boolean we can just return here (without value) and it breaks the loop
-                    isGuessed = true;
+                    return;
                 } else {
-                    System.out.println("Sorry, try again!");
+                    attempts++;
+                    String tries = attempts == 1 ? "attempt" : "attempts";
+                    System.out.printf("Sorry, you used %d %s, try again!%n", attempts, tries);
                 }
             }
-        }
 
-        // Second option (easier to read) - but app can blow up if user will provide not q or integer
-        while(true) {
-            guessedNumberStr = System.console().readLine("Please put a number between 1 and 10 inclusively %n");
-            if (guessedNumberStr.equals("q")) {
-                return;
-            }
-
-            int guessedNumber = Integer.parseInt(guessedNumberStr);
-
-            if (guessedNumber == rndNum) {
-                System.out.println("You've got it!");
-                return;
-            } else {
-                System.out.println("Sorry, try again!");
-            }
-        }
+        } while (!"q".equals(guessedNumberStr));
     }
 }
