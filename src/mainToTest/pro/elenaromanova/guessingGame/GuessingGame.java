7e1035cf97df1c27ed3918f6e75fb628d7edebf8
Elena <for-alisia@yandex.ru>
2023-01-18 19:13:07 +0100

    Added more tests for guessing game

--- src/mainToTest/pro/elenaromanova/guessingGame/GuessingGame.java
+++ src/mainToTest/pro/elenaromanova/guessingGame/GuessingGame.java
@@ -4,8 +4,16 @@ import java.util.Random;
 
 public class GuessingGame {
     private final int rndNumber = new Random().nextInt(10) + 1;
+    private int counter = 0;
     public String guess(int guessNum) {
-        return guessNum == getRndNumber() ? "You got it!" : "Please try again!";
+        counter++;
+        boolean isGuessed = guessNum == getRndNumber();
+        if (counter >= 4 && !isGuessed) {
+            return "The game is over. You used all tries";
+        }
+        String tryText = counter == 1 ? "try" : "tries";
+        String msg = String.format("You got it in %d %s!", counter, tryText);
+        return isGuessed ? msg : "Please try again!";
     }
 
     public int getRndNumber() {
--- src/test/pro/elenaromanova/guessingGame/GuessingGameTest.java
+++ src/test/pro/elenaromanova/guessingGame/GuessingGameTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GuessingGameTest {
 
     private GuessingGame game;
+    private final int RND_GENERATOR_TRIES = 100;
 
     @BeforeEach
     void setUp() {
@@ -20,7 +21,7 @@ public class GuessingGameTest {
         int rndNumber = game.getRndNumber();
         String msg = game.guess(rndNumber);
 
-        assertEquals("You got it!", msg);
+        assertEquals("You got it in 1 try!", msg);
     }
 
     @Test
@@ -42,7 +43,7 @@ public class GuessingGameTest {
     @RepeatedTest(5)
     public void testRandomNumberGeneration() {
         int[] arr = new int[11];
-        for (int counter = 0; counter < 60; counter++) {
+        for (int counter = 0; counter < RND_GENERATOR_TRIES; counter++) {
             GuessingGame newGame = new GuessingGame();
             int rndNumber = newGame.getRndNumber();
             arr[rndNumber] = 1;
@@ -54,5 +55,37 @@ public class GuessingGameTest {
         }
 
         assertEquals(10, sum);
+    }
+
+    @Test
+    public void testFourWrongGuesses() {
+        String result = "";
+        for (int i = 0; i < 4; i++) {
+            result = game.guess(-3);
+        }
+
+        assertEquals("The game is over. You used all tries", result);
+    }
+
+    @Test
+    public void testThreeWrongAndCorrectGuesses() {
+        String result = "";
+        for (int i = 0; i < 4; i++) {
+            int guess =  i == 3 ? game.getRndNumber() : -3;
+            result = game.guess(guess);
+        }
+
+        assertEquals("You got it in 4 tries!", result);
+    }
+
+    @Test
+    public void testTwoWrongAndCorrectGuesses() {
+        String result = "";
+        for (int i = 0; i < 3; i++) {
+            int guess =  i == 2 ? game.getRndNumber() : -3;
+            result = game.guess(guess);
+        }
+
+        assertEquals("You got it in 3 tries!", result);
     }
 }
